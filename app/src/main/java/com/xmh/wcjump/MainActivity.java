0f15207bd9378bb25163c1951aaffd418dd059c5
xmh <xmh19936688@163.com>
2018-01-07 12:52:13 +0800

    add send-to-server

--- app/src/main/java/com/xmh/wcjump/MainActivity.java
+++ app/src/main/java/com/xmh/wcjump/MainActivity.java
@@ -9,13 +9,20 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String IP = "192.168.42.87";
+
+    // 悬浮框
     private int windowWidth;
     private int windowHeight;
     private View windowView;
     private boolean isWindowShowing = false;
 
+    // 点击位置
     private int clickX;
     private int clickY;
 
@@ -34,8 +41,10 @@ public class MainActivity extends AppCompatActivity {
         clickX = screenWidth / 2;
         clickY = screenHeight * 5 / 6;
 
+        // 初始化悬浮窗
         initWindow();
 
+        // 开启/关闭悬浮窗
         findViewById(R.id.v).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,7 +53,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**初始化悬浮窗*/
     private void initWindow() {
+        // layout params
         WindowManager.LayoutParams params = new WindowManager.LayoutParams();
         params.type = WindowManager.LayoutParams.TYPE_TOAST;
         params.format = PixelFormat.TRANSLUCENT;
@@ -53,11 +64,20 @@ public class MainActivity extends AppCompatActivity {
         params.width = windowWidth;
         params.height = windowHeight;
 
+        // inflate view
         windowView = View.inflate(this, R.layout.window, null);
         windowView.setTag("max");
         windowView.setLayoutParams(params);
 
+        // handle touch
         windowView.findViewById(R.id.control).setOnTouchListener(new View.OnTouchListener() {
+
+            // click position
+            private float startX;
+            private float startY;
+            private float endX;
+            private float endY;
+
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 int action = motionEvent.getActionMasked();
@@ -65,18 +85,32 @@ public class MainActivity extends AppCompatActivity {
                     return false;
                 }
 
-                float x = motionEvent.getX();
-                float y = motionEvent.getY();
-                Log.e("xmh", "touch2||" + "action:" + MotionEvent.actionToString(action) + x + "*" + y);
+                // start x y
+                if (action == MotionEvent.ACTION_DOWN) {
+                    startX = motionEvent.getX();
+                    startY = motionEvent.getY();
+                }
+                // end x y
+                if (action == MotionEvent.ACTION_UP) {
+                    endX = motionEvent.getX();
+                    endY = motionEvent.getY();
+                    int delta = (int) Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
+                    // send to server
+                    send(delta);
+                }
                 return true;
             }
         });
+
+        // 开启/关闭悬浮窗
         windowView.findViewById(R.id.close).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 toggleWindow();
             }
         });
+
+        // 折叠到左边
         windowView.findViewById(R.id.left).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,6 +135,8 @@ public class MainActivity extends AppCompatActivity {
                 windowManager.updateViewLayout(parent, params);
             }
         });
+
+        // 折叠到右边
         windowView.findViewById(R.id.right).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -127,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**开启/关闭悬浮窗*/
     private void toggleWindow() {
         if (isWindowShowing) {
             isWindowShowing = false;
@@ -137,6 +174,38 @@ public class MainActivity extends AppCompatActivity {
             WindowManager windowManager = (WindowManager) getApplicationContext().getSystemService(WINDOW_SERVICE);
             windowManager.addView(windowView, windowView.getLayoutParams());
         }
+    }
+
+    /**向server发数据*/
+    private void send(final int delta) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection conn = null;
+                try {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("http://").append(IP).append("/jump");
+                    sb.append("?x=").append(clickX);
+                    sb.append("&y=").append((clickY));
+                    sb.append("&d=").append(delta);
+                    Log.e("xmh", "url:" + sb.toString());
+
+                    URL url = new URL(sb.toString());
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(false);
+                    conn.setDoOutput(false);
+                    conn.setUseCaches(false);
+                    conn.setRequestMethod("GET");
+                    int code = conn.getResponseCode();
+                    Log.e("xmh","resp:"+code);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (conn != null) conn.disconnect();
+                }
+            }
+        }).start();
     }
 
 }
